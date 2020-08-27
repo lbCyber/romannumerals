6301c4466f801860bcd962c297c84e929b25e7fe
lbCyber <paul@lintbox.com>
2020-08-27 18:32:32 -0400

    Cleaned up a bit, fixed a few minor issues, added basic error handling

--- roman.js
+++ roman.js
@@ -1,17 +1,11 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   document.getElementById("numbers").value = ""
   const RomanNumerals = {}
-  const numWrite = [
-    [1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]
-  ] // Array read in order to write RN
-  const numRead = [
-    [900, "CM"], [400, "CD"], [90, "LC"], [40, "XL"], [9, "IX"], [4, "IV"], [1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]
-  ] // Array read in order to read RN
-
+  RomanNumerals.lib = [[[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]], [[900, "CM"], [400, "CD"], [90, "LC"], [40, "XL"], [9, "IX"], [4, "IV"], [1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]]
   RomanNumerals.toRoman = (v) => {
     let res = []; // Answer string
     if (Number.isInteger(v)) {
-      numWrite.forEach((val) => {
+      RomanNumerals.lib[0].forEach((val) => {
         while (Math.floor(v / val[0]) > 0) {
           res.push(val[1])
           v -= val[0]
@@ -19,7 +13,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
       })
       return (res.join("").toUpperCase())
     } else {
-      console.log(`${v} - ${Number.isInteger(v)}`)
       return "Not an arabic number!"
     }
   }
@@ -28,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     if (Number.isInteger(v) === false) {
       let str = v.toUpperCase() // Remainder of string to parse
       let res = 0; // Answer value
-      numRead.forEach((val, key) => {
+      RomanNumerals.lib[1].forEach((val) => {
         const checkLoop = () => {
           let int = str.search(val[1])
           if (int > -1) {
@@ -39,17 +32,20 @@ document.addEventListener('DOMContentLoaded', function (event) {
         }
         checkLoop()
       })
-      console.log(`${v} - ${Number.isInteger(v)}`)
       return res
     } else {
-      console.log(`${v} - ${Number.isInteger(v)}`)
       return "Not a roman numeral!"
     }
   }
   document.getElementById("submit").addEventListener("click", () => {
-    let oldNum = parseInt(document.getElementById("numbers").value)
-    let newNum = ""
+    let oldNum
+    let newNum
     let checked = document.getElementById("encorDec").checked
+    if (document.getElementById("numbers").value >= 0) {
+      oldNum = parseInt(document.getElementById("numbers").value)
+    } else {
+      oldNum = document.getElementById("numbers").value
+    }
     if (checked) {
       newNum = RomanNumerals.toRoman(oldNum)
     } else {
