f8d4e8b69d5c2fd2aa57285541577af21bd69979
lbCyber <paul@lintbox.com>
2020-08-27 18:42:45 -0400

    Cleaned up code a bit, removed a few redundancies

--- roman.js
+++ roman.js
@@ -1,7 +1,6 @@
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', function (e) {
   document.getElementById("numbers").value = ""
-  const RomanNumerals = {}
-  RomanNumerals.lib = [[[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]], [[900, "CM"], [400, "CD"], [90, "LC"], [40, "XL"], [9, "IX"], [4, "IV"], [1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]]
+  const RomanNumerals = {"lib": [[[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]], [[900, "CM"], [400, "CD"], [90, "LC"], [40, "XL"], [9, "IX"], [4, "IV"], [1000, "M"], [500, "D"], [100, "C"], [50, "L"], [10, "X"], [5, "V"], [1, "I"]]]}
   RomanNumerals.toRoman = (v) => {
     let res = []; // Answer string
     if (Number.isInteger(v)) {
@@ -11,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
           v -= val[0]
         }
       })
-      return (res.join("").toUpperCase())
+      return (res.join(""))
     } else {
       return "Not an arabic number!"
     }
@@ -19,14 +18,13 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
   RomanNumerals.fromRoman = (v) => {
     if (Number.isInteger(v) === false) {
-      let str = v.toUpperCase() // Remainder of string to parse
       let res = 0; // Answer value
       RomanNumerals.lib[1].forEach((val) => {
         const checkLoop = () => {
-          let int = str.search(val[1])
+          let int = v.search(val[1])
           if (int > -1) {
             res += val[0]
-            str = str.replace(val[1], "")
+            v = v.replace(val[1], "")
             checkLoop() // Repeat check if still more equating letter values
           }
         }
@@ -44,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     if (document.getElementById("numbers").value >= 0) {
       oldNum = parseInt(document.getElementById("numbers").value)
     } else {
-      oldNum = document.getElementById("numbers").value
+      oldNum = document.getElementById("numbers").value.toUpperCase()
     }
     if (checked) {
       newNum = RomanNumerals.toRoman(oldNum)
